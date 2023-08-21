59f1f955050d8291ab86358475ed19026376ef7e
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-08-21 15:13:36 +0300

    chore: add tsconfig & tsup to build the project

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+import tsconfig from './tsconfig.json';
+
+export default defineConfig((options) => ({
+  name: 'Axiom',
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  target: tsconfig.compilerOptions.target as 'esnext',
+  outDir: 'dist',
+  dts: true,
+  clean: true,
+  minify: !options.watch,
+}));
